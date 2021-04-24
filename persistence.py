1b10b5971f25bf0b5b89402b8299483d2ca34501
edr <github@rochefort.de>
2021-04-24 15:12:10 +0200

    Remove Flask-SQLAlchemy

--- persistence.py
+++ persistence.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from sqlalchemy import DateTime, ForeignKey, create_engine, Table, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, relationship, scoped_session, sessionmaker
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker
 
 Base = declarative_base()
 
