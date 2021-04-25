af01b764ea928f5c211301224f6f357c43d588ac
edr <github@rochefort.de>
2021-04-25 18:46:03 +0200

    Coverage script

---
+++ run_coverage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $(dirname $0)
+export PYTHONPATH="$(pwd):$(pwd)/tests:$(pwd)/venv/lib/python3.9/site-packages/"
+source ./venv/bin/activate
+cd ./tests
+coverage3 run --source ../ --omit */venv/* -m unittest discover .
+coverage3 report -m
