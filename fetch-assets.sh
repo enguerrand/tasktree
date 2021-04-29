11aa3961bca8fdb2a5ab65b7b4050eb446d4fe47
edr <github@rochefort.de>
2021-04-29 21:04:03 +0200

    Host bootstrap css locally

--- fetch-assets.sh
+++ fetch-assets.sh
@@ -20,19 +20,20 @@ fi
 mode="$1"
 [ -z "${mode}" ] && abort "Missing argument: mode"
 
-reactjs_baseurl="https://unpkg.com/"
 reactjs_version=17.0.2
+bootstrap_version=4.3.1
 
 cd "$(dirname $0)"
 
 rm -rf assets/js
-mkdir -p assets/js
+mkdir -p assets/js assets/css
 if [ "$mode" == "prod" ]; then
-    wget "${reactjs_baseurl}react@${reactjs_version}/umd/react.production.min.js" -N -O assets/js/react.js
-    wget "${reactjs_baseurl}react-dom@${reactjs_version}/umd/react-dom.production.min.js" -N -O assets/js/react-dom.js
+    wget "https://unpkg.com/react@${reactjs_version}/umd/react.production.min.js" -N -O assets/js/react.js
+    wget "https://unpkg.com/react-dom@${reactjs_version}/umd/react-dom.production.min.js" -N -O assets/js/react-dom.js
 elif [ "$mode" == "dev" ]; then 
-    wget "${reactjs_baseurl}react@${reactjs_version}/umd/react.development.js" -N -O assets/js/react.js
-    wget "${reactjs_baseurl}react-dom@${reactjs_version}/umd/react-dom.development.js" -N -O assets/js/react-dom.js
+    wget "https://unpkg.com/react@${reactjs_version}/umd/react.development.js" -N -O assets/js/react.js
+    wget "https://unpkg.com/react-dom@${reactjs_version}/umd/react-dom.development.js" -N -O assets/js/react-dom.js
 fi
 
+wget https://stackpath.bootstrapcdn.com/bootstrap/${bootstrap_version}/css/bootstrap.min.css -P assets/css/
 wget https://raw.githubusercontent.com/enguerrand/reactjs-helpers/master/reactjs-helpers.js -P assets/js/
